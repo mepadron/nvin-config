51efae0fdfdf56fe505d322b08b29c7bc7825c0d
mepadron <mikyeduards@gmail.com>
2025-08-14 17:16:30 -0400

    add flutter run terminal

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -8,4 +8,13 @@ return {
   config = function()
     require("flutter-tools").setup({})
   end,
+  keys = {
+    {
+      "<leader>fr",
+      function()
+        vim.fn.jobstart('gnome-terminal -- bash -c "flutter run; read -p \"Press Enter to close terminal...\" "')
+      end,
+      desc = "Flutter Run",
+    },
+  },
 }
--- lua/plugins/vim-test.lua
+++ lua/plugins/vim-test.lua
@@ -9,6 +9,11 @@ return {
     vim.keymap.set("n", "<leader>a", ":TestSuite<CR>", {})
     vim.keymap.set("n", "<leader>l", ":TestLast<CR>", {})
     vim.keymap.set("n", "<leader>g", ":TestVisit<CR>", {})
-    vim.cmd("let test#strategy = 'vimux'")
+    vim.g['test#custom_strategies'] = {
+      gnome_terminal = function(cmd)
+        vim.fn.jobstart('gnome-terminal -- bash -c "' .. cmd .. '; read -p \'Press Enter to close terminal...\' "')
+      end
+    }
+    vim.g['test#strategy'] = 'gnome_terminal'
   end,
 }
