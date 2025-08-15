eeb80f7d00ccf337e46d57a5da6bd9155d4dad6f
mepadron <mikyeduards@gmail.com>
2025-08-15 09:52:23 -0400

    ajustes ' cl

--- lua/plugins/react-native.lua
+++ lua/plugins/react-native.lua
@@ -5,7 +5,10 @@ return {
     {
       "<leader>rn",
       function()
-        vim.fn.jobstart('gnome-terminal -- bash -c "npm run android; read -p \"Press Enter to close terminal...\" "')
+        local current_dir = vim.fn.expand('%:p:h')
+        -- Usar el comando xdg-open para abrir la terminal predeterminada
+        local cmd = string.format('gnome-terminal -- bash -c "cd %s && npm run android; exec bash"', vim.fn.shellescape(current_dir))
+        vim.fn.jobstart(cmd, { detach = true })
       end,
       desc = "React Native Run",
     },
